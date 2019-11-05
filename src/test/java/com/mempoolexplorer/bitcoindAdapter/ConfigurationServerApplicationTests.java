af5ec5fc68c9fbd15682a151b9ee0e12d6e4ce42
kk <a@a.com>
2019-11-05 14:11:45 +0100

    Commit inicial con todos los ficheros

---
+++ src/test/java/com/mempoolexplorer/bitcoindAdapter/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mempoolexplorer.bitcoindAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
