5c8f19cf4a30ce72cb74c36e379d3c13c2cfc4ca
kk <a@a.com>
2019-11-05 16:22:30 +0100

    Renombrada paquetería

--- src/main/java/com/mempoolexplorer/configuration/server/ConfigurationServerApplication.java
+++ src/main/java/com/mempoolexplorer/configuration/server/ConfigurationServerApplication.java
@@ -1,4 +1,4 @@
-package com.mempoolexplorer.bitcoindAdapter;
+package com.mempoolexplorer.configuration.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
