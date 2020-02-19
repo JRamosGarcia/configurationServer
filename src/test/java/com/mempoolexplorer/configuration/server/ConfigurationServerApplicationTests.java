3958f675d9f7d2f91c126d0acbe21c6c1706989f
jramos <juanramosgarcia@protonmail.com>
2020-02-19 11:05:39 +0100

    Added configuration for mempoolRecorder

--- src/test/java/com/mempoolexplorer/configuration/server/ConfigurationServerApplicationTests.java
+++ src/test/java/com/mempoolexplorer/configuration/server/ConfigurationServerApplicationTests.java
@@ -1,13 +1,12 @@
 package com.mempoolexplorer.configuration.server;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConfigurationServerApplicationTests {
-
+/*
 	@Test
 	void contextLoads() {
 	}
-
+*/
 }
